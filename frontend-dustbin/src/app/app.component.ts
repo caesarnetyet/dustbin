825325abef4f1c6d0fbb7ee7dc317630b0ddae3c
caesarnetyet <caesarnetyet@gmail.com>
2023-04-12 16:20:54 -0600

    adding flowbite

--- frontend-dustbin/src/app/app.component.ts
+++ frontend-dustbin/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  initAccordions,
+  initCarousels,
+  initCollapses,
+  initDials,
+  initDismisses,
+  initDrawers,
+  initDropdowns,
+  initModals,
+  initPopovers,
+  initTabs,
+  initTooltips,
+} from 'flowbite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-dustbin';
+  
+  ngOnInit(): void {
+    initAccordions();
+    initCarousels();
+    initCollapses();
+    initDials();
+    initDismisses();
+    initDrawers();
+    initDropdowns();
+    initModals();
+    initPopovers();
+    initTabs();
+    initTooltips();
+  }
 }
--- frontend-dustbin/src/app/app.module.ts
+++ frontend-dustbin/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './session/features/login/login.component';
+import { RegisterComponent } from './session/features/register/register.component';
+import { MenuComponent } from './session/features/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
