638008a0ee4f1446565830eadafbfe97532cb8fe
caesar <caesarwenli@gmail.com>
2023-04-14 22:34:16 -0600

    rest

--- frontend-dustbin/src/app/services/error-handler.service.ts
+++ frontend-dustbin/src/app/services/error-handler.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,18 @@ import { Injectable } from '@angular/core';
 export class ErrorHandlerService {
 
   constructor() { }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
