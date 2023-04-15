bcdd18ee33aacf5b83edec0f9610d80a6cb5967d
caesar <caesarwenli@gmail.com>
2023-04-14 20:51:35 -0600

    enviroment

--- frontend-dustbin/src/app/session/data-access/session.service.ts
+++ frontend-dustbin/src/app/session/data-access/session.service.ts
@@ -1,14 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
+import { Observable, catchError } from 'rxjs';
+import { UserRegister } from 'src/shared/interfaces/requests.interface';
+import { API_URL } from 'src/app/env';
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor(http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-
+  registerUser(request: any): Observable<UserRegister> {
+    return this.http.post<UserRegister>(API_URL + "/user", request)
+      .pipe(
+        catchError(this.handleError<UserRegister>('register'))
+      )
+  }
 
 }
