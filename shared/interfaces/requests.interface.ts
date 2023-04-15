3278b6d1c15c2a135d10c426f9fa703a0cad7f3a
caesar <caesarwenli@gmail.com>
2023-04-14 20:56:41 -0600

    git gud

--- shared/interfaces/requests.interface.ts
+++ shared/interfaces/requests.interface.ts
@@ -1,3 +1,16 @@
 export interface UserRegister {
+  user: User;
+  token: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  role_id: number
+  created_at: string;
+  updated_at: string;
 
 }
