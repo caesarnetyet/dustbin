d3305205dfb9b42a9a216f9d3251c0a9368ac165
Abelardo <abelardoreyes256@gmail.com>
2023-04-18 13:29:16 -0600

    Se termino el crud de modelo_sensor

--- backend-dustbin/app/Models/Model_Sensor.ts
+++ backend-dustbin/app/Models/Model_Sensor.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
-export default class Model_User extends BaseModel {
+export default class Model_Sensor extends BaseModel {
 
   @column({ isPrimary: true })
   public id: number
