92dacaef06df38c9af557e8377296cb4054cbed7
Abelardo <abelardoreyes256@gmail.com>
2023-04-18 11:40:07 -0600

    Controlador Admin con CRUD

--- backend-dustbin/app/Controllers/Http/AdminsController.ts
+++ backend-dustbin/app/Controllers/Http/AdminsController.ts
@@ -16,8 +16,8 @@ export default class AdminsController {
       phone: schema.string(),
     })
     const payload = await request.validate({ schema: newUserSchema })
-    if(!payload){
-      return {message: 'Error al guardar el usuario'}
+    if (!payload) {
+      return { message: 'Error al guardar el usuario' }
     }
     const user = new User()
     user.name = payload.name
@@ -38,17 +38,22 @@ export default class AdminsController {
   public async updateUser({ request }: HttpContextContract) {
     const updateUserSchema = schema.create({
       name: schema.string(),
-      email: schema.string(),
-      password: schema.string(),
+      email: schema.string({}, [
+        rules.email(),
+        rules.unique({ table: 'users', column: 'email' }),
+      ]),
+      password: schema.string({}, [
+        rules.confirmed(),
+      ]),
       phone: schema.string(),
     })
     const payload = await request.validate({ schema: updateUserSchema })
-    if(!payload){
-      return {message: 'Error al actualizar el usuario'}
+    if (!payload) {
+      return { message: 'Error al actualizar el usuario' }
     }
     const findUser = await User.find(request.params().id)
-    if(!findUser){
-      return {message: 'No se encontró el usuario'}
+    if (!findUser) {
+      return { message: 'No se encontró el usuario' }
     }
     findUser.name = payload.name
     findUser.email = payload.email
@@ -60,10 +65,10 @@ export default class AdminsController {
 
   public async deleteUser({ request }: HttpContextContract) {
     const findUser = await User.find(request.params().id)
-    if(!findUser){
-      return {message: 'No se encontró el usuario'}
+    if (!findUser) {
+      return { message: 'No se encontró el usuario' }
     }
     await findUser.delete()
-    return {message: 'Usuario eliminado'}
+    return { message: 'Usuario eliminado' }
   }
 }
