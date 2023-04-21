0691e5d964ca7ec2c48be445c02e8d2d54e25313
Abelardo <abelardoreyes256@gmail.com>
2023-04-21 00:33:49 -0600

    Mongo watch

---
+++ backend-dustbin/app/Controllers/Conection/MongoConection.mjs
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+const uri = "mongodb+srv://admin:admin@cluster0.urq1yx8.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db("Prueba");
+    const collection = db.collection("sensors");
+
+    const changeStream = collection.watch();
+    changeStream.on("change", (change) => {
+      console.log(change);
+
+    }
+    );
+  }
+  catch (err) {
+    console.log("Error al conectar con la base de datos");
+  }
+}
+
+run()
